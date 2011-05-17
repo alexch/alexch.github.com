66322be3c03cc88c64c526789bebf232422f6143
Alex Chaffee <alex@stinky.com>
2011-05-17 12:03:26 -0700

    google analytics

--- index.rb
+++ index.rb
@@ -137,6 +137,24 @@ a:hover { color: red; }
     link :rel=>"stylesheet", :href=>"http://f.fontdeck.com/s/css/u5mYSdgdXljzmDHdstX1xDoEPik/alexch.github.com/1443.css", :type=>"text/css"
   end
   
+  def self.google_analytics_code account_id
+    <<-JAVASCRIPT
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '#{account_id}']);
+    _gaq.push(['_setDomainName', 'none']);
+    _gaq.push(['_setAllowLinker', true]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    JAVASCRIPT
+  end
+
+  external :script, google_analytics_code('UA-23417120-1')
+  
   def body_content
     # div :id => "right_side" do
     #   twitter

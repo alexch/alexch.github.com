1a422c5e79dae2929fbf56582d72e9fa93423de3
Alex Chaffee <alex@stinky.com>
2015-02-06 14:46:29 -0500

    add an alexism

--- alexisms.rb
+++ alexisms.rb
@@ -31,10 +31,16 @@ class Alexisms < Index
         p '(Feathers: "Legacy" means "no tests.")'
       end
 
+      say "The only perfect program is an empty file."
+
       say 'If you try hard enough, you can make anything fail.'
+
       say 'There\'s no such thing as human error. (Only system error.)'
+
       say 'If you pay attention to something, it gets better.'
+
       say 'It\'s always a people problem. (Jerry Weinberg)'
+
       say 'You can see a lot by looking. (Yogi Berra)' do
         p 'Yogi wrote a book called "You Can Observe A Lot By Watching" but I prefer to think he was misquoting himself.'
         p do
@@ -42,18 +48,24 @@ class Alexisms < Index
           a :href => '/web/20120526011007/http://itre.cis.upenn.edu/~myl/languagelog/archives/005375.html' do
             text 'She was seeing at me'
           end
+          text '.'
         end
       end
+
       say 'Object-Oriented Programming is like teenage sex: everyone says they\'re doing it; few actually are; and those who are rarely know what they\'re doing. (Anonymous, via Misko)' do
         p 'Here\'s a simple test for whether you\'re doing it right: Is your data in the same class as the methods accessing it? Oh, really? Check again.'
       end
+
       say 'Double negatives are not unconfusing.'
+
       say 'Encapsulation means putting similar things together, and keeping dissimilar things apart.' do
         p 'Of course, the trick of design is knowing along what axes to group or differentiate items. One rule of thumb that has served me well since the days of Gamelan -- when we were sorting dozens of incoming applets per day into categories -- is:'
       end
+
       say 'Don\'t look at the item and think, "What category does this item belong in? Look at the category and think, "If I were looking for items in this category, would I want to find this item?"' do
         p 'In other words, make your API fit the mindset of the user, not that of the provider.'
       end
+
       div.saying do
         h3 do
           a :href => '/web/20120526011007/http://en.wikipedia.org/wiki/Conway\'s_Law' do
@@ -63,10 +75,13 @@ class Alexisms < Index
         end
         p 'Or, "The structure of the code reflects the structure of the coders."'
       end
+
       say 'Read the error message.' do
         p "Even if it looks like barf, there are pearls in there, and you have to feel for them."
       end
+
       say '"Have strong opinions, weakly held." - Paul Saffo'
+
     end
   end
 end
